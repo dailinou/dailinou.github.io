226eb883a5e7aa043733e09ae1ebd4c71f47cb83
Linou <dailinou@umich.edu>
2018-04-16 11:15:12 -0400

    final_exam

---
+++ webDesignExam/jquery.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  $('.hide').click(function(){
+    $('.pic1').hide();
+    $('.image_container').css({
+      'justify-content':'center'
+    });
+  });
+
+  $('.show').click(function(){
+    $('.pic1').show();
+  })
+
+
+
+});
